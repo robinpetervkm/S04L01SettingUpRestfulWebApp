d761c2d8ae4ea70606c249753ebe63a7ae8fd24c
Robinclear <robin.peter.vkm@gmail.com>
2020-04-15 18:50:11 -0300

    First Commit

---
+++ src/main/java/com/norha/S04L01SettingUpRestfulWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.norha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04L01SettingUpRestfulWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04L01SettingUpRestfulWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/norha/S04L01SettingUpRestfulWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04L01SettingUpRestfulWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
